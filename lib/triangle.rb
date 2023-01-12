684b1807891f1c99c6dec52d7697d36c62040495
john Caropino <jccaropino7786@live.com>
2023-01-12 08:23:25 -0800

    commit tree 478019e300c74aedd0b332b3c89580db45c93b9b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
-class Triangle
-  # write code here
+class Triangle 
+  attr_reader :side_one, :side_two, :side_three
+
+  def initialize( side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    if side_one==side_two && side_two==side_three
+      :equilateral
+    elsif side_one==side_two or side_two==side_three or side_one==side_three
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    (side_one,side_two,side_three).all > 0
+    
+  end
+  
+  class TriangleError < StandardError
+
+  end
 end
